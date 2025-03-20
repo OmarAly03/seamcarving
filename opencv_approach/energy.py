6ab818888332f4d6012831fbf5001514e95703b4
OmarAly03 <omar.aly@ejust.edu.eg>
2025-03-21 00:35:49 +0200

    d

--- opencv_approach/energy.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cv2
-
-def compute_energy(gray):
-    """Computes the energy map using |dI/dx| + |dI/dy| with OpenCV Sobel"""
-    dx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=3)  # Gradient in x
-    dy = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=3)  # Gradient in y
-    energy = np.abs(dx) + np.abs(dy)  # Energy = |dx| + |dy|
-    return energy
